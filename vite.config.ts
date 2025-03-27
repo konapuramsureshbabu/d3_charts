c76c0b922e398900730daa45f94b675f78934ade
Sureshbabu Konapuram <114557970+konapuramsureshbabu@users.noreply.github.com>
2025-03-27 22:48:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/d3_charts/',
   plugins: [react()],
+  base: '/d3_charts/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    port: 5173
+  }
 })
